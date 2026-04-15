2f152f711136254f9142e5273116dcce0561e689
Citrine-tns <Djocovid19@gmail.com>
2026-04-15 20:28:07 +0900

    Initial Commit

---
+++ ShukatsuCalendar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShukatsuCalendar
+//
+//  Created by 井田和志 on 2026/04/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShukatsuCalendar/ShukatsuCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShukatsuCalendarApp.swift
+//  ShukatsuCalendar
+//
+//  Created by 井田和志 on 2026/04/15.
+//
+
+import SwiftUI
+
+@main
+struct ShukatsuCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
